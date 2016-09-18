8a0f3dd52823afe85bdbd61597d14fa8a55cc4bc
Hồ Xuân Thường <hothixuanthuong1408@gmail.com>
2016-09-18 22:22:40 +0700

    Ngay 1
    Ma nguon dau tien

---
+++ Danhsach.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSSV
+{
+    class DS
+    {
+        string ho;
+        string ten;
+        string lop;
+        string maSV;
+
+        public DS(string ho, string ten, string lop, string maSV)
+        {
+            this.ho = ho;
+            this.ten = ten;
+            this.lop = lop;
+            this.maSV = maSV;
+        }
+
+        public void SetHo(string ho)
+        {
+            this.ho = ho;
+        }
+        public string GetHo()
+        {
+            return this.ho;
+        }
+
+        public void SetTen(string ten)
+        {
+            this.ten = ten;
+        }
+
+        public string GetTen()
+        {
+            return this.ten;
+        }
+
+        public void SetLop(string lop)
+        {
+            this.lop = lop;
+        }
+
+        public string GetLop()
+        {
+            return this.lop;
+        }
+        public void SetMa(string maSV)
+        {
+            this.maSV = maSV;
+        }
+
+        public string GetMa()
+        {
+            return this.maSV;
+        }
+    }
+}
